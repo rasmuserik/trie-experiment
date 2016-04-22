0a41720e09f207ce0d4377830574af596c7990e8
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-04-22 09:58:56 +0200

    trie-experiment2 running

--- trie-experiment2.js
+++ trie-experiment2.js
@@ -7,6 +7,10 @@ if(!Object.assign) {
     }
   }
 }
+
+var n = 1000000;
+var radix = 20;
+var maxLinearTrie = 30;
 ////////////////////////////////////////////////////////////////////
 
 function Trie() { throw "abstract"; }
@@ -27,12 +31,16 @@ Trie.prototype.get = function(s) {
   return trie.val;
 };
 Trie.prototype.print = function(n) {
-  this._print("", n);
+  this._print("", n || 1000000000);
 };
 Trie.prototype._print = function(s, n) {
   if(n <= 0) { return n; }
   if(this.val !== undefined) { console.log(s, this.val); --n; }
-  return this.__print(s, n);
+  var list = this.list();
+  for(var i = 0; i < list.length; ++i) {
+    var c = list[i];
+    n = this.next(c)._print(s + String.fromCharCode(c), n);
+  }
 };
 
 function str2arr(s) {
@@ -42,33 +50,117 @@ function str2arr(s) {
   }
   return arr;
 }
-function addVal(o, val) { o.val = val; return o; }
 
+//
 function EmptyTrie(val) { this.val = val;};
 Object.assign(EmptyTrie.prototype, Trie.prototype);
 EmptyTrie.prototype.next = function(c) { return emptyTrie; }
-EmptyTrie.prototype.add = function(c, trie) { return new PrefixTrie(c, trie, this.val); }
-EmptyTrie.prototype.addVal = function(val) { return addVal(new EmptyTrie(), val); }
-EmptyTrie.prototype.__print = function(s, n) { return n};
+EmptyTrie.prototype.add = function(c, trie) { 
+  return new PrefixTrie(c, trie, this.val); 
+}
+EmptyTrie.prototype.addVal = function(val) { 
+  return new EmptyTrie(val);
+}
+EmptyTrie.prototype.list = function() { return emptyList; };
 var emptyTrie = new EmptyTrie();
+var emptyList = [];
 
-function PrefixTrie(c, trie, val) { this.c = c; this.trie = trie; this.val = val; }
+//
+function PrefixTrie(c, trie, val) { 
+  this.c = c; this.trie = trie; this.val = val; 
+}
 Object.assign(PrefixTrie.prototype, Trie.prototype);
-PrefixTrie.prototype.next = function(c) { return c === this.c ? this.trie : emptyTrie; }
+PrefixTrie.prototype.next = function(c) { 
+  return c === this.c ? this.trie : emptyTrie; 
+}
 PrefixTrie.prototype.add = function(c, trie) { 
   if(c === this.c) {
     return new PrefixTrie(c, trie, this.val);
   } else {
-    return multiTrieAdd(multiTrieAdd(new MultiTrie(_emptyArr, this.val), this.c, this.trie), c, trie); 
+    if(c < this.c) {
+      return new BinaryTrie(c, this.c, trie, this.trie, this.val);
+    } else {
+      return new BinaryTrie(this.c, c, this.trie, trie, this.val);
+    }
   }
-}
-PrefixTrie.prototype.addVal = function(val) { return addVal(new PrefixTrie(this.c, this.trie, this.val), val); };
-PrefixTrie.prototype._print = function(s, n) { return this.trie._print(s + String.fromCharCode(this.c), n); }
+};
+PrefixTrie.prototype.addVal = function(val) { 
+  return new PrefixTrie(this.c, this.trie, this.val);
+};
+PrefixTrie.prototype.list = function() { return [this.c]; };
 
+//
+function BinaryTrie(a, b, A, B, val) { 
+  this.a = a, this.b = b, this.A = A, this.B = B, this.val = val;}
+  Object.assign(BinaryTrie.prototype, Trie.prototype);
+  BinaryTrie.prototype.next = function(c) { 
+    return this.a === c ? this.A : (this.b === c ? this.B : emptyTrie);
+  }
+BinaryTrie.prototype.addVal = function(val) { 
+  return new BinaryTrie(this.a, this.b, this.A, this.B, this.val);
+};
+BinaryTrie.prototype.add = function(c, trie) {
+  if(this.a === c) {
+    return new BinaryTrie(this.a, this.b, trie, this.B, this.val);
+  }
+  if(this.b === c) {
+    return new BinaryTrie(this.a, this.b, this.A, trie, this.val);
+  }
+  return new LinearTrie([this.a, this.b], 
+      [this.A, this.B], this.val).add(c, trie);
+};
+BinaryTrie.prototype.list = function() { return [this.a, this.b]; };
+
+
+//
+function LinearTrie(cs, tries, val) { this.cs = cs, this.tries = tries; this.val = val; }
+Object.assign(LinearTrie.prototype, Trie.prototype);
+LinearTrie.prototype.next = function(c) {
+  var cs = this.cs;
+  for(var i = 0; i < cs.length; ++i) {
+    if(cs[i] === c) {
+      return this.tries[i];
+    }
+  }
+  return emptyTrie;
+}
+LinearTrie.prototype.add = function(c, trie) {
+  var pos;
+  var cs = this.cs;
+  var tries = this.tries;
+  for(pos = 0; pos < cs.length; ++pos) {
+    if(c <= cs[pos]) {
+      break;
+    }
+  }
+  if(cs[pos] === c) {
+    tries = tries.slice();
+    tries[pos] = trie;
+    return new LinearTrie(cs, tries, this.val);
+  } 
+  var cs = cs.slice(0, pos).concat([c], cs.slice(pos));
+  var tries = tries.slice(0, pos).concat([trie], tries.slice(pos));
+
+  if(cs.length > maxLinearTrie) {
+    return makeMultiTrie(cs, tries, this.val);
+  } else {
+    return new LinearTrie(cs, tries, this.val);
+  }
+};
+LinearTrie.prototype.list = function() { return this.cs; };
+
+//
+function makeMultiTrie(cs, tries, val) {
+  var result = new MultiTrie(_emptyArr, val);
+  for(var i = 0; i < cs.length; ++i) {
+    multiTrieAdd(result, cs[i], tries[i]);
+  }
+  return result;
+}
 function MultiTrie(arr, val) { this.arr = arr; this.val = val; }
 function multiTrieAdd(multiTrie, c, trie) {
   prevArr = multiTrie.arr
-  var hibits = (c >> 4);
+    var hibits = (c >> 4);
   var arr = prevArr.slice();
   var loArr = arr[hibits] = prevArr[hibits].slice();
   loArr[c & 15] = trie;
@@ -78,14 +170,15 @@ function multiTrieAdd(multiTrie, c, trie) {
 Object.assign(MultiTrie.prototype, Trie.prototype);
 MultiTrie.prototype.next = function(c) { return this.arr[c >> 4][c & 15]; }
 MultiTrie.prototype.add = function(c, trie) { return multiTrieAdd(new MultiTrie(this.arr, this.val), c, trie); }
-MultiTrie.prototype.addVal = function(val) { return addVal(new MultiTrie(this.arr, this.val), val); }
-MultiTrie.prototype.__print = function(s, n) {
+MultiTrie.prototype.addVal = function(val) { return new MultiTrie(this.arr, this.val); };
+MultiTrie.prototype.list = function() {
+  result = [];
   for(var i = 0; i < 16; ++i) {
-    for(var j = 0; j < 16; ++j) {
-      if(this.arr[i][j] !== emptyTrie) { n = this.arr[i][j]._print(s + String.fromCharCode((i << 4) + j), n) }
-    }
-  }
-  return n;
+    if(this.arr[i] !== _e) {
+      for(var j = 0; j < 16; ++j) {
+        if(this.arr[i][j] !== emptyTrie) {
+          result.push((i << 4) + j); } } } }
+  return result;
 }
 var _et = emptyTrie;
 _e = [_et, _et, _et, _et, _et, _et, _et, _et, _et, _et, _et, _et, _et, _et, _et, _et] 
@@ -95,13 +188,22 @@ t = emptyTrie.insert('hello', 1);
 t = t.insert('world', 2);
 t = t.insert('word', 3);
 //t = t.insert('ba', 2);
-t.print();
+t.print(10);
 
 // ----------------
 
-var n = 480000;
 function s(i) {
+  /*
+  result = 'hello';
+  while(i) {
+    result += String.fromCharCode(65 + (i % radix));
+    i = (i / radix) |0;
+  }
+
+  //return result;
+  //*/
   return "h" + i;
+
 }
 
 var t0 = Date.now();
@@ -132,6 +234,6 @@ for(var i = 0; i < n; ++i) {
 }
 console.log("trie get time: ", Date.now() - t0, sum);
 
-trie.print(10);
+//trie.print(30);
 /*
 */
